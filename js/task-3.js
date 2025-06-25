155293f463c2f35a8aa572fade205dce3d92950d
unknown <you@example.comomelchuk.8787@gmail.com>
2025-06-25 19:45:56 +0300

    all

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,24 @@
-function getElementWidth(content, padding, border) {
-    let contentWidts = parseFloat(content);
-    let paddingWidts = parseFloat(padding);
-    let borderWidts = parseFloat(border);
-    return contentWidts + paddingWidts * 2 + borderWidts * 2;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+  
+    changeUsername(newName) {
+     this.username = newName;
+    }, 
+
+    updatePlayTime(hours) {
+     this.playTime += hours;
+    },
+    
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
